455437dc152634142649cb120b9a571968309b57
Alireza Shahamiri <shahamiri.alireza81@gmail.com>
2023-08-25 17:56:45 +0330

    feat(accounts): Creates custom signup for CustomRegisterSerializer

--- core/accounts/serializers.py
+++ core/accounts/serializers.py
@@ -1,19 +1,13 @@
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from rest_framework import serializers
+from .models import User
 
 
 class CustomRegisterSerializer(RegisterSerializer):
     first_name = serializers.CharField(max_length=150, required=True)
     last_name = serializers.CharField(max_length=150, required=True)
 
-    def get_cleaned_data(self):
-        return {
-            "username": self.validated_data.get("username", ""),
-            "password1": self.validated_data.get("password1", ""),
-            "email": self.validated_data.get("email", ""),
-            "first_name": self.validated_data.get("first_name", ""),
-            "last_name": self.validated_data.get("last_name", ""),
-        }
-
-    def save(self, request):
-        return super().save(request)
+    def custom_signup(self, request, user):
+        user.first_name = self.validated_data.get("first_name")
+        user.last_name = self.validated_data.get("last_name")
+        user.save(update_fields=["first_name", "last_name"])
