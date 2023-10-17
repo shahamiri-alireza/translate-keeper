67350d92e673b51c81ecc4453f26408064e22ff7
Alireza Shahamiri <shahamiri.alireza81@gmail.com>
2023-10-18 01:04:30 +0330

    feat(camelCase): Adds camelCase renderer to project

--- core/core/settings.py
+++ core/core/settings.py
@@ -132,6 +132,19 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
+    "DEFAULT_RENDERER_CLASSES": (
+        "core.utils.camelize_renderer.CamelizeRenderer",
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        # Any other renders
+    ),
+    "DEFAULT_PARSER_CLASSES": (
+        # If you use MultiPartFormParser or FormParser, we also have a camel case version
+        "djangorestframework_camel_case.parser.CamelCaseFormParser",
+        "djangorestframework_camel_case.parser.CamelCaseMultiPartParser",
+        "djangorestframework_camel_case.parser.CamelCaseJSONParser",
+        # Any other parsers
+    ),
 }
 
 AUTH_USER_MODEL = "accounts.User"
---
+++ core/core/utils/camelize_renderer.py
@@ -0,0 +1,30 @@
+from rest_framework.renderers import JSONRenderer
+from rest_framework import status
+from djangorestframework_camel_case.render import CamelCaseJSONRenderer
+
+
+class CamelizeRenderer(JSONRenderer):
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        status_code = renderer_context["response"].status_code
+        exception = renderer_context["response"].exception
+
+        response = {
+            "success": status_code and str(status_code).startswith("2"),
+            "code": status_code if status_code else status.HTTP_400_BAD_REQUEST,
+            "message": data.pop("detail")
+            if isinstance(data, dict) and data.get("detail")
+            else "",
+        }
+
+        if exception:
+            response["key"] = data.get("key") if "key" in data else ""
+            response["errors"] = data.get("errors") if "errors" in data else data
+        else:
+            response["data"] = (
+                data.get("data") if isinstance(data, dict) and "data" in data else data
+            )
+
+        camelize = CamelCaseJSONRenderer().render(
+            response, accepted_media_type, renderer_context
+        )
+        return camelize
