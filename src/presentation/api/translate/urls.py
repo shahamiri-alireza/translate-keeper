42b03bef1d7d7c6a91c7a5e50901546323ef9f08
Alireza Shahamiri <shahamiri.alireza81@gmail.com>
2023-11-05 00:28:51 +0330

    feat(urls): Adds category url

--- src/presentation/api/translate/urls.py
+++ src/presentation/api/translate/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from presentation.controllers.translate.dictionary import DictionaryModelViewSet
+from presentation.controllers.translate.category import CategoryModelViewSet
 from presentation.controllers.translate.language import LanguageViewSet
 
 router = DefaultRouter()
 router.register("dictionary", DictionaryModelViewSet, basename="/dictionary")
+router.register("category", CategoryModelViewSet, basename="category")
 
 urlpatterns = [
     path("language/list/", LanguageViewSet.as_view({"get": "list"}), name="language")
