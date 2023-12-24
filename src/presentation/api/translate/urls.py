a0a6f4b92cbe24b0eb4c20e93102f25c0443b7c4
Alireza Shahamiri <shahamiri.alireza81@gmail.com>
2023-12-25 01:40:01 +0330

    feat(word): Adds word route to urls

--- src/presentation/api/translate/urls.py
+++ src/presentation/api/translate/urls.py
@@ -4,10 +4,12 @@ from rest_framework.routers import DefaultRouter
 from presentation.controllers.translate.dictionary import DictionaryModelViewSet
 from presentation.controllers.translate.category import CategoryModelViewSet
 from presentation.controllers.translate.language import LanguageViewSet
+from presentation.controllers.translate.word import WordModelViewSet
 
 router = DefaultRouter()
 router.register("dictionary", DictionaryModelViewSet, basename="/dictionary")
 router.register("category", CategoryModelViewSet, basename="category")
+router.register("word", WordModelViewSet, basename="word")
 
 urlpatterns = [
     path("language/list/", LanguageViewSet.as_view({"get": "list"}), name="language")
