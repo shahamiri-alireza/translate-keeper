e3e5da6ee763eb2c5769f286630c0893e9e208c5
Alireza Shahamiri <shahamiri.alireza81@gmail.com>
2024-01-21 22:36:02 +0330

    fix(Translation): Fixes translation model word on_delete

--- src/domain/apps/translate/models/translation.py
+++ src/domain/apps/translate/models/translation.py
@@ -5,7 +5,7 @@ from . import Language, Word
 
 
 class Translation(BaseModel):
-    word = models.ForeignKey(Word, on_delete=models.PROTECT, blank=False, null=False)
+    word = models.ForeignKey(Word, on_delete=models.CASCADE, blank=False, null=False)
     translate = models.CharField(max_length=255, blank=False, null=False)
     language = models.ForeignKey(
         Language, on_delete=models.PROTECT, blank=False, null=False
